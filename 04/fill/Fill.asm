51cbadb5362cdea3420f1588d6583e59c20c1cae
extremq <stefansteff999@gmail.com>
2022-07-02 19:06:00 +0300

    Project 4 complete.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,43 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(REPEAT)
+    @8191 // 32 * 256 places to fill
+    D=A   // Copy the value to place it in a counter
+    @0
+    M=D   // Counter is set on R0
+    (FILL)
+        @0
+        D=M // Get counter value
+        @REPEAT
+        D;JLT // check if counter is negative (meaning we reached the end)
+        @1    // If it isnt, we compute in R1 the address  
+        M=D   // R1 = COUNTER + OFFSET (16384)
+        @16384
+        D=A
+        @1
+        M=M+D
+        
+        @KBD  // check for keypress
+        D=M
+        @WHITE
+        D;JEQ // Equal to zero will jump to white. 
+        @1    // Didn't jump = Pressed a key
+        D=M   // Take the required address and place a -1 for black
+        A=D
+        M=-1
+        @BLACK
+        0;JEQ
+        (WHITE)
+        @1    // Jumped, meaning nothing was pressed
+        D=M
+        A=D
+        M=0   // Place a 0
+        (BLACK)
+        @0
+        M=M-1      // decrement the counter
+        @FILL      // repeat
+        0;JMP
+
+    @REPEAT // Loop infinitely
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,23 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @2
+    M=0
+(REPEAT)
+    @0
+    D=M // Store R0 in D
+    
+    @END
+    D;JEQ // If R0 = 0 jump to END
+
+    @0
+    M=M-1 // Subtract 1 from R0
+    
+    @1
+    D = M // Store R1 in D
+    @2
+    M = M + D // Add R1 to R2
+
+    @REPEAT
+    0;JMP
+(END)
